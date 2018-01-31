ad2411e81acdd9df359483cbe3ba2edb0c5021dc
DESKTOP-CN8NUDP\RIM <ghl2682@gmail.com>
2018-01-31 10:53:13 -0500

    header and favicon images

--- js/json/projects_parsing.js
+++ js/json/projects_parsing.js
@@ -1,15 +1,6 @@
 function parsingProjectsForIndex() {
     var htm = "";
     $.each($.parseJSON(projects), function () {
-     /*
-        htm += "<div class='col-lg-12'><div class='thumbnail' id='imageSection'>" +
-            "<img src=" + this.imgurl + " alt ='projects image' height ="+this.height+"width="+this.width+">" +
-            "</div> <div class='caption' id='imageSection'><h3>" +this.caption+"</h3><p>"+this.description+"</p></div></div><br><br><br>";
-    */
-    /*        htm += "<div id='imageCaptionDiv'><div id='projectImageDiv'>" +
-            "<img src=" + this.imgurl + " alt ='projects image' height ="+this.height+"width="+this.width+">" +
-            "</div> <div id='projectCaptionDiv'><h4>" +this.caption+"</h4><p>"+this.description+"</p></div></div>";
-*/
 
    htm += "<div class='col-lg-12' id='imageCaptionDiv'>"+
    "<div class='thumbnail'>" +
